f091297aadfbb51563875b39eca3d661f5072974
hai <hai16112001kk@gmail.com>
2023-05-12 18:18:54 +0700

    first

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <navBar />
+    <router-view />
+  </div>
+</template>
+
+
+<script>
+
+import navBar from './components/navBar.vue'
+export default {
+  name: 'App',
+  components: { navBar }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
